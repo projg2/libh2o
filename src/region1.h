ced030bddd4888ba334ded042dc5381c3f176492
Michał Górny <mgorny@gentoo.org>
2012-03-07 22:07:05 +0100

    Add gamma expr for region 1.

---
+++ src/region1.h
@@ -0,0 +1,22 @@
+/* libh2o -- steam & water properties
+ * (c) 2012 Michał Górny
+ * Released under the terms of the 2-clause BSD license
+ */
+
+#pragma once
+
+#ifndef _H2O_REGION1_H
+#define _H2O_REGION1_H 1
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /*__cplusplus*/
+
+double h2o_region1_gamma_pt(double p, double T);
+
+#ifdef __cplusplus
+};
+#endif /*__cplusplus*/
+
+#endif /*_H2O_REGION1_H*/
