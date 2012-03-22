6205c61355aa6e9e8fc0b54bb2a072c31e40b9fd
Michał Górny <mgorny@gentoo.org>
2012-03-22 14:35:36 +0100

    Region 4: use R2 metastable equations for p <= 10 MPa.

--- src/region4.c
+++ src/region4.c
@@ -11,20 +11,28 @@
 #include "region2.h"
 #include "saturation.h"
 
+/* Create values by interpolating R1 & R2
+ * for p <= 10 MPa, use R2-meta instead */
+
 double h2o_region4_v_Tx(double T, double x) /* [K, 0..1] -> [m³/kg] */
 {
 	double p = h2o_saturation_p_T(T);
 
 	if (x == 0)
 		return h2o_region1_v_pT(p, T);
-	else if (x == 1)
-		return h2o_region2_v_pT(p, T);
 	else
 	{
-		double v1 = h2o_region1_v_pT(p, T);
-		double v2 = h2o_region2_v_pT(p, T);
+		double v2 = p <= 10 ? h2o_region2_meta_v_pT(p, T)
+				: h2o_region2_v_pT(p, T);
 
-		return v1 + (v2 - v1) * x;
+		if (x == 1)
+			return v2;
+		else
+		{
+			double v1 = h2o_region1_v_pT(p, T);
+
+			return v1 + (v2 - v1) * x;
+		}
 	}
 }
 
@@ -34,14 +42,19 @@ double h2o_region4_u_Tx(double T, double x) /* [K, 0..1] -> [kJ/kg] */
 
 	if (x == 0)
 		return h2o_region1_u_pT(p, T);
-	else if (x == 1)
-		return h2o_region2_u_pT(p, T);
 	else
 	{
-		double u1 = h2o_region1_u_pT(p, T);
-		double u2 = h2o_region2_u_pT(p, T);
+		double u2 = p <= 10 ? h2o_region2_meta_u_pT(p, T)
+				: h2o_region2_u_pT(p, T);
 
-		return u1 + (u2 - u1) * x;
+		if (x == 1)
+			return u2;
+		else
+		{
+			double u1 = h2o_region1_u_pT(p, T);
+
+			return u1 + (u2 - u1) * x;
+		}
 	}
 }
 
@@ -51,14 +64,19 @@ double h2o_region4_s_Tx(double T, double x) /* [K, 0..1] -> [kJ/kgK] */
 
 	if (x == 0)
 		return h2o_region1_s_pT(p, T);
-	else if (x == 1)
-		return h2o_region2_s_pT(p, T);
 	else
 	{
-		double s1 = h2o_region1_s_pT(p, T);
-		double s2 = h2o_region2_s_pT(p, T);
+		double s2 = p <= 10 ? h2o_region2_meta_s_pT(p, T)
+				: h2o_region2_s_pT(p, T);
 
-		return s1 + (s2 - s1) * x;
+		if (x == 1)
+			return s2;
+		else
+		{
+			double s1 = h2o_region1_s_pT(p, T);
+
+			return s1 + (s2 - s1) * x;
+		}
 	}
 }
 
@@ -68,13 +86,18 @@ double h2o_region4_h_Tx(double T, double x) /* [K, 0..1] -> [kJ/kg] */
 
 	if (x == 0)
 		return h2o_region1_h_pT(p, T);
-	else if (x == 1)
-		return h2o_region2_h_pT(p, T);
 	else
 	{
-		double h1 = h2o_region1_h_pT(p, T);
-		double h2 = h2o_region2_h_pT(p, T);
+		double h2 = p <= 10 ? h2o_region2_meta_h_pT(p, T)
+				: h2o_region2_h_pT(p, T);
 
-		return h1 + (h2 - h1) * x;
+		if (x == 1)
+			return h2;
+		else
+		{
+			double h1 = h2o_region1_h_pT(p, T);
+
+			return h1 + (h2 - h1) * x;
+		}
 	}
 }
