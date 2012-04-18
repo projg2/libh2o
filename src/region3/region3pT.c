4ab90941a58e03b4a8a949e028a840f378cbae22
Michał Górny <mgorny@gentoo.org>
2012-04-18 18:08:30 +0200

    Region 3: commonize out finding region 3k.

--- src/region3/region3pT.c
+++ src/region3/region3pT.c
@@ -112,6 +112,8 @@ enum h2o_region3_subregion_pT h2o_region3_subregion_pT(double p, double T)
 		else /*if (T <= h2o_region3jk_T_p(p))*/
 			return H2O_REGION3R_PT;
 	}
+	else if (T > h2o_region3ij_T_p(p))
+		return H2O_REGION3J_PT;
 	else if (p > 23)
 	{
 		if (T <= h2o_region3gh_T_p(p))
@@ -123,10 +125,8 @@ enum h2o_region3_subregion_pT h2o_region3_subregion_pT(double p, double T)
 		}
 		else if (T <= h2o_region3ef_T_p(p))
 			return H2O_REGION3H_PT;
-		else if (T <= h2o_region3ij_T_p(p))
+		else /*if (T <= h2o_region3ij_T_p(p))*/
 			return H2O_REGION3I_PT;
-		else /*if (T <= h2o_region3jk_T_p(p))*/
-			return H2O_REGION3J_PT;
 	}
 	else /*if (p > 22.5)*/
 	{
@@ -138,10 +138,8 @@ enum h2o_region3_subregion_pT h2o_region3_subregion_pT(double p, double T)
 			return H2O_REGION3N_PT;
 		else if (T <= h2o_region3op_T_p(p))
 			return H2O_REGION3O_PT;
-		else if (T <= h2o_region3ij_T_p(p))
+		else /*if (T <= h2o_region3ij_T_p(p))*/
 			return H2O_REGION3P_PT;
-		else /*if (T <= h2o_region3jk_T_p(p))*/
-			return H2O_REGION3J_PT;
 	}
 }
 
